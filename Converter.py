d2148effe6326cbc782ffe7bd918c9feffda2cb0
karina_mermaid <karinasog96@gmail.com>
2020-09-23 09:45:12 +0300

    after_PEP

--- Converter.py
+++ Converter.py
@@ -5,15 +5,15 @@ while True:
     from_what_convert = str(input('Enter F or C:'))
     to_what_convert = str(input('Enter F or C:'))
     if from_what_convert == 'F' and to_what_convert == 'C':
-        print(round( (number * (9/5) + 32), 2))
+        print(round((number * (9/5) + 32), 2))
         break
     elif from_what_convert == 'C' and to_what_convert == 'F':
-        print( round(((number - 32) * 5/9), 2))
+        print(round(((number - 32) * 5/9), 2))
         break
     elif from_what_convert == 'F' and to_what_convert == 'F' or from_what_convert == 'C' and to_what_convert == 'F':
-        print ('There is nothing to convert, try again')
+        print('There is nothing to convert, try again')
         continue
     else :
-        print ('You typed something wrong, try again')
+        print('You typed something wrong, try again')
         continue
 
